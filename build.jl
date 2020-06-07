95df7a97c22f8571fc0ce5fb09c3e7cacf013513
Daniel Lakeland <dlakelan@street-artists.org>
2020-06-07 16:39:15 -0700

    Added a COVID data monitoring notebook, including the ipynb so we can shove it into Binder easily.

--- build.jl
+++ build.jl
@@ -3,8 +3,10 @@
 using Weave
 
 docs = [ #"0_Prerequisites.jmd", ## don't build this as it installs packages
-        "BasicDataAndPlots.jmd",
-        "DiscussionBasicDataAndPlots.jmd"];
+         "BasicDataAndPlots.jmd",
+         "DiscussionBasicDataAndPlots.jmd",
+         "COVID-monitoring.jmd"
+         ];
 
 for i in docs 
     notebook(i);
